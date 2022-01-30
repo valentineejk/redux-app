b65986279d96ddcf8b69b3370d2b379d93fd0ac8
miskay <miskayapp@gmail.com>
2022-01-30 23:38:38 +0100

    v1.1

---
+++ src/features/user.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const initialValuest={name: "",email: "",age: 0};
+
+export const userSlice=createSlice({
+    name: "user",
+    initialState: {value: initialValuest},
+    reducers: {
+        login: (state,action) => {
+            state.value=action.payload;
+        },
+        logout: (state) => {
+            state.value=initialValuest;
+        },
+    },
+
+
+})
+
+export const {login,logout}=userSlice.actions;
+
+// const User=() => {
+//     return <div></div>;
+// };
+
+export default userSlice.reducer;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+import userReducer from './features/user'
+
+const store=configureStore({
+  reducer: {
+    user: userReducer,
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <Provider store={store}>
+      <App />
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
